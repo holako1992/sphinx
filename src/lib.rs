ceffb7a847b455ba635b76287f8cbc78cb8c2baf
dev <holako92@protonmail.com>
2025-12-20 15:03:39 +0400

    added payload format

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@ pub mod crypto;
 pub mod header;
 pub mod packet;
 pub mod payload;
+pub mod payload_format;
 pub mod route;
 pub mod surb;
 mod utils;
@@ -37,4 +38,5 @@ pub use crate::error::{Error, ErrorKind, Result};
 pub use crate::packet::{
     builder::SphinxPacketBuilder, ProcessedPacket, ProcessedPacketData, SphinxPacket,
 };
+pub use crate::payload_format::{ForwardPayload, PacketType, ReplyPayload};
 pub use crate::surb::{SURBMaterial, SURB};
